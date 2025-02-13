d070bb258b97ac29411ff45e5762c8f7aaaeda4c
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-02-13 10:43:48 -0800

    enemy added

--- index.js
+++ index.js
@@ -1,145 +1,191 @@
-//defines height and width of canvas
-var canvasWidth = 600;
-var canvasHeight = 400;
+// Frank Poth 08/13/2017
 
-//defines pkayer and y position
-var player;
-var playerYPosition = 200;
-
-//fall speed stuff
-var fallSpeed = 0;
-var interval = setInterval(updateCanvas, 10);
-
-//if jumping stuff
-var isJumping = false;
-var jumpSpeed = 0;
+var context, controller, player, loop;
 
 
+context = document.querySelector("canvas").getContext("2d");
+
+context.canvas.height = innerHeight -20;
+context.canvas.width = innerWidth - 20;
+
+player = {
+
+  height:64,
+  jumping:true,
+  width:64,
+  x:144, // center of the canvas
+  x_velocity:0,
+  y: 0,
+  y_velocity:0
+
+};
 
 
-//starts the game and creates the player
-function startGame() {
-    gameCanvas.start();
+enemy = {
 
-    //width, height, x
-    player = new createPlayer(30, 30, 10);
-    
-}
+    height:32,
+    jumping:true,
+    width:32,
+    x:144, 
+    x_velocity:0,
+    y:931,
+    y_velocity:0
+  };
+var ground = context.canvas.height - player.y - player.height;
+controller = {
 
-//creates the game canvas
-var gameCanvas = {
-    canvas: document.createElement("canvas"),
-    start: function() {
-        this.canvas.width = canvasWidth;
-        this.canvas.height = canvasHeight;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  left:false,
+  right:false,
+  up:false,
+  keyListener:function(event) {
+
+    var key_state = (event.type == "keydown");
+
+    switch(event.keyCode) {
+      case 37:// left key
+        controller.left = key_state;
+        break;
+    case 65:
+        controller.left = key_state;
+        break;
+      case 32:
+        controller.up = key_state;
+        break;
+      case 87:
+        controller.up = key_state;
+        break;  
+      case 38:// up key
+        controller.up = key_state;
+        break;
+      case 39:// right key
+        controller.right = key_state;
+        break;
+    case 68:
+        controller.right = key_state;
+        break;
+
     }
-}
 
-//creates the player
-function createPlayer(width, height, x) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = playerYPosition;
+  }
+
+};
+
+loop = function() {
+
+  if (controller.up && player.jumping == false) {
+
+    player.y_velocity -= 25;
+    player.jumping = true;
+
+  }
+
+  if (controller.left) {
+
+    player.x_velocity -= 1;
+
+  }
+
+  if (controller.right) {
+
+    player.x_velocity += 1;
+
+  }
+
+  player.y_velocity += 1.5;// gravity
+  player.x += player.x_velocity;
+  player.y += player.y_velocity;
+  player.x_velocity *= 0.9;// friction
+  player.y_velocity *= 0.9;// friction
+
+
+
+  // if player is falling below floor line
+
+
+
+
+
+  if (player.y > ground) {
+
+    player.jumping = false;
+    player.y = ground;
+    player.y_velocity = 0;
+
+  }
+
+  
+
+  
+
+
+  // if player is going off the left of the screen
+
+
+    context.fillStyle = "#202020";
+    context.fillRect(0, 0, innerWidth, innerHeight);// x, y, width, height
+    context.fillStyle = "#ff0000";
+    context.beginPath();
+    context.rect(player.x, player.y, player.width, player.height);
+    context.fill();
+    context.fillStyle = "blue";
+    context.beginPath();
+    context.rect(enemy.x, enemy.y, enemy.width, enemy.height);
+    context.fill();
+  
     
 
-    //draws the canvas
-    this.draw = function() {
-        ctx = gameCanvas.context;
-        ctx.fillStyle = "green";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.makeFall = function() {
-        if (!isJumping) {
-            this.y += fallSpeed;
-            fallSpeed += 0.1;
-            this.stopPlayer();
+
+  if (collision({ player, enemy })) {
+    console.log("HIT");
+  }
+  // call update when the browser is ready to draw again
+  window.requestAnimationFrame(loop);
+
+};
+
+window.addEventListener("keydown", controller.keyListener)
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
+
+
+
+
+
+
+
+function collision({ player, enemy }) {
+    if (player.x < enemy.x + enemy.width && player.x + player.width > enemy.x &&
+        player.y < enemy.y + enemy.height && player.y + player.height > enemy.y) {
+        if (player.y + player.height > enemy.y && player.y_velocity > 0) {
+            player.jumping = false;
+            player.y_velocity = 0;
+            player.y = enemy.y - player.height;
+
+    
         }
-    }
-    this.stopPlayer = function() {
-        var ground = canvasHeight - this.height;
-        if (this.y > ground) {
-            this.y = ground;
-        }
-    }
-    this.jump = function() {
-        if (isJumping) {
-            this.y -= jumpSpeed;
-            jumpSpeed += 1;
-        }
-    }
-    
-}
+        else{
 
-
-
-
-
-
-function updateCanvas() {
-
-    
-    ctx = gameCanvas.context;
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    
-    player.makeFall();
-    player.draw();
-    player.jump();
-    
-    
-    
-
-    
-}
-
-
-
-function resetJump() {
-    jumpSpeed = 0;
-    isJumping = false;
-}
-
-document.body.onkeyup = function(e) {
-    if (e.keyCode == 32) {
-        isJumping = true;
-        //defines how long you jumo
-        setTimeout(function() { resetJump(); }, 500);
-    }
-}
-
-
-
-let isMoving = false;
-let id;
-
-document.body.onkeydown = function(e){
-    if(e.keyCode == 68 && !isMoving){
-        isMoving = true;
-        id = setInterval(frame, 20);
-       
-    }
-}
-
-document.body.onkeyup = function(e){
-    if(e.keyCode == 68){
-        isMoving = false;
-        clearInterval(id);
-    }
-}
-
-
-function frame() {
-    if (isMoving) {
-        player.x = player.x + 10;
-    }
-}
-
-
-document.body.onkeyup = function(e){
         
-    if(e.keyCode == 68){
-        
+            if (player.x + player.width > enemy.x && player.x < enemy.x + enemy.width) {
+                if (player.x_velocity > 0) { // Moving to the right
+                    player.x = enemy.x - player.width;  // Set player position to the left of the enemy
+                } else if (player.x_velocity < 0) { // Moving to the left
+                    player.x = enemy.x + enemy.width;  // Set player position to the right of the enemy
+                }
+                player.x_velocity = 0;
+            }
+        }
     }
 }
+
+
+
+
+//player.x < enemy.x + enemy.width right side
+
+
+//player.y < enemy.y + enemy.height bottom
+
+//player.x + player.width > enemy.x left side
+
+//player.y + player.height > enemy.y top
